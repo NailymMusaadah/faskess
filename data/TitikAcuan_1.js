b0a8f20e12226c863b9520dacbdb955211debc54
NailymMusaadah <75231074+NailymMusaadah@users.noreply.github.com>
2020-12-06 19:44:43 +0700

    Add files via upload

---
+++ data/TitikAcuan_1.js
@@ -0,0 +1,15 @@
+var json_TitikAcuan_1 = {
+"type": "FeatureCollection",
+"name": "TitikAcuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.876867933813941, -7.404760710248967 ] } },
+{ "type": "Feature", "properties": { "id": "2" }, "geometry": { "type": "Point", "coordinates": [ 110.998831716072601, -7.358683389357371 ] } },
+{ "type": "Feature", "properties": { "id": "3" }, "geometry": { "type": "Point", "coordinates": [ 111.023224472524319, -7.426645752612914 ] } },
+{ "type": "Feature", "properties": { "id": "4" }, "geometry": { "type": "Point", "coordinates": [ 110.861767656010514, -7.331034697672794 ] } },
+{ "type": "Feature", "properties": { "id": "5" }, "geometry": { "type": "Point", "coordinates": [ 111.087110263231239, -7.312601282478719 ] } },
+{ "type": "Feature", "properties": { "id": "5" }, "geometry": { "type": "Point", "coordinates": [ 110.948884643338104, -7.305688555467387 ] } },
+{ "type": "Feature", "properties": { "id": "6" }, "geometry": { "type": "Point", "coordinates": [ 111.096402741879515, -7.473867653771471 ] } },
+{ "type": "Feature", "properties": { "id": "7" }, "geometry": { "type": "Point", "coordinates": [ 110.936107485196729, -7.365595293313975 ] } }
+]
+}
